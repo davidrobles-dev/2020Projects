35709d365173d5237ee85a86b5de1f2d63d5ed87
davidrobles-dev <daverobles0527@gmail.com>
2020-09-19 19:01:38 +0800

    Update main.py
    Add tkinter main loop and to start window design

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# Import Python Library
 import os
-from tkinter import tkinter
-print(dir(os))
+from tkinter import *
+
+# Initialize the Tkinter window
+userInterface = Tk()
+
+userInterface.title("File Management System")
+
+browsedir = Button(userInterface, background="white" , text ="Browse")
+browsedir.pack(side = LEFT)
+
+# Loop the user-interface
+userInterface.mainloop()
+
+
