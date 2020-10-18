b95ecabc0d5fea3144474970e616ec8032b272ad
davidrobles-dev <daverobles0527@gmail.com>
2020-10-19 06:01:33 +0800

    Create function.py
    Added function.py which will house library that will be used during the app.

---
+++ function.py
@@ -0,0 +1,7 @@
+# 1. Add a product
+#   Be able to look into for the excel file or create it
+#   Add the product name
+#   Put the product on an existing category
+#   Add a product description
+#   Add Stock Unit, dimension (lenght and width), and weight (kg)
+#   Add unit price
