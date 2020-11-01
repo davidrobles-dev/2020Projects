2e782b563ca659ff35c56cf6772d07b7a42fd845
davidrobles-dev <daverobles0527@gmail.com>
2020-11-01 19:27:27 +0800

    Edited function.py and add Users.txt
    Import os and created a label for the UI

--- function.py
+++ function.py
@@ -8,11 +8,22 @@
 #                       Importing needed library
 # ********************************************************************
 from tkinter import *
+import os
 
 # Initialize window name
 log = Tk()
 log.title("Masiter Flower Shop") # Name of the app window
 log.geometry("1000x600") # Userinterface W x L 
+
+# Setup the window widgets
+def placeLabel(Ui,Greetings):
+    Label(Ui,text= Greetings + "\n").pack()
+
+# Check for the file or create the file that served as database
+logfile = open("Users.txt","w+")
+
+placeLabel(log,"Welcome to Trident PoS System!")
+placeLabel(log,"123")
 
 # Loop the user-interface
 log.mainloop()
